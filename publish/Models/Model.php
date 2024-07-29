1101d02748d16ac5ea913131f8610bf7fb5fb0e0
Gijs Jorissen <mail@gijsjorissen.nl>
2024-07-29 10:03:45 +0200

    Remove whereLike scope

--- publish/Models/Model.php
+++ publish/Models/Model.php
@@ -7,9 +7,4 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
 class Model extends EloquentModel
 {
     protected $guarded = [];
-
-    public function scopeWhereLike($query, $column, $value)
-    {
-        return $query->where($column, 'like', "%{$value}%");
-    }
 }
