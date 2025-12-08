edb9cb6d9c9f6673921c3f5fc2ea44add1d99834
Gijs Jorissen <mail@gijsjorissen.nl>
2025-12-08 16:21:04 +0100

    wip

--- publish/failed-job-monitor/config/failed-job-monitor.php
+++ publish/failed-job-monitor/config/failed-job-monitor.php
@@ -2,30 +2,17 @@
 
 use Awssat\Notifications\Channels\DiscordWebhookChannel;
 
+/**
+ * https://github.com/spatie/laravel-failed-job-monitor/blob/main/config/failed-job-monitor.php
+ */
 return [
 
-    /*
-     * The notification that will be sent when a job fails.
-     */
     'notification' => App\Support\FailedJobMonitor\Notification::class,
 
-    /*
-     * The notifiable to which the notification will be sent. The default
-     * notifiable will use the mail and slack configuration specified
-     * in this config file.
-     */
     'notifiable' => App\Support\FailedJobMonitor\Notifiable::class,
 
-    /*
-     * By default notifications are sent for all failures. You can pass a callable to filter
-     * out certain notifications. The given callable will receive the notification. If the callable
-     * return false, the notification will not be sent.
-     */
     'notificationFilter' => null,
 
-    /*
-     * The channels to which the notification will be sent.
-     */
     'channels' => [DiscordWebhookChannel::class],
 
     'mail' => [
@@ -39,4 +26,5 @@ return [
     'discord' => [
         'webhook_url' => env('FAILED_JOB_DISCORD_WEBHOOK_URL'),
     ],
+
 ];
