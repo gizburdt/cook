7f975e387c2a0b81e256fd20eadb544d4780e4df
Gijs Jorissen <mail@gijsjorissen.nl>
2025-12-10 19:28:28 +0100

    wip

--- src/Composer.php
+++ src/Composer.php
@@ -6,10 +6,8 @@ use Illuminate\Support\Composer as BaseComposer;
 
 class Composer extends BaseComposer
 {
-    public function installPackages(
-        array $packages,
-        string $extra = ''
-    ): int {
+    public function installPackages(array $packages, string $extra = ''): int
+    {
         $extra = $extra ? (array) $extra : [];
 
         $packages = collect($packages)->prepend('require')->toArray();
@@ -21,61 +19,39 @@ class Composer extends BaseComposer
 
     public function addScript(string $hook, string $script): int
     {
-        $currentScripts = $this->getScripts($hook);
-
-        if (in_array($script, $currentScripts)) {
-            return 0;
-        }
-
-        $currentScripts[] = $script;
-
-        // todo: use composer config scripts.x "value"
-        return $this->setScripts($hook, $currentScripts);
+        return $this->addToConfig("scripts.{$hook}", $script);
     }
 
     public function addAutoloadFile(string $file): int
     {
-        $currentFiles = $this->getAutoloadFiles();
+        return $this->addToConfig('autoload.files', $file);
+    }
 
-        if (in_array($file, $currentFiles)) {
+    public function getFromConfig(string $key): array
+    {
+        $config = $this->getComposerConfig();
+
+        return (array) (data_get($config, $key) ?? []);
+    }
+
+    protected function addToConfig(string $key, string $value): int
+    {
+        $current = $this->getFromConfig($key);
+
+        if (in_array($value, $current)) {
             return 0;
         }
 
-        $currentFiles[] = $file;
+        $current[] = $value;
 
-        return $this->setAutoloadFiles($currentFiles);
+        return $this->setConfig($key, $current);
     }
 
-    public function getAutoloadFiles(): array
+    protected function setConfig(string $key, mixed $value): int
     {
         $config = $this->getComposerConfig();
 
-        return $config['autoload']['files'] ?? [];
-    }
-
-    protected function setAutoloadFiles(array $files): int
-    {
-        $config = $this->getComposerConfig();
-
-        $config['autoload']['files'] = $files;
-
-        return $this->writeComposerConfig($config);
-    }
-
-    public function getScripts(string $hook): array
-    {
-        $config = $this->getComposerConfig();
-
-        $scripts = $config['scripts'][$hook] ?? [];
-
-        return (array) $scripts;
-    }
-
-    protected function setScripts(string $hook, array $scripts): int
-    {
-        $config = $this->getComposerConfig();
-
-        $config['scripts'][$hook] = $scripts;
+        data_set($config, $key, $value);
 
         return $this->writeComposerConfig($config);
     }
